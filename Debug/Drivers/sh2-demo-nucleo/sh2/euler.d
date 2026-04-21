0db82ab012fbf25f421ed1df4495a63f2b8b49dd
TML1006 <trevorlane1006@gmail.com>
2026-04-21 00:07:52 -0700

    Updated files to account for ESP32 pivot

--- Debug/Drivers/sh2-demo-nucleo/sh2/euler.d
+++
@@ -1,4 +0,0 @@
-Drivers/sh2-demo-nucleo/sh2/euler.o: \
- ../Drivers/sh2-demo-nucleo/sh2/euler.c \
- ../Drivers/sh2-demo-nucleo/sh2/euler.h
-../Drivers/sh2-demo-nucleo/sh2/euler.h:
--- Debug/Drivers/sh2-demo-nucleo/sh2/sh2_util.d
+++
@@ -1,4 +0,0 @@
-Drivers/sh2-demo-nucleo/sh2/sh2_util.o: \
- ../Drivers/sh2-demo-nucleo/sh2/sh2_util.c \
- ../Drivers/sh2-demo-nucleo/sh2/sh2_util.h
-../Drivers/sh2-demo-nucleo/sh2/sh2_util.h:
