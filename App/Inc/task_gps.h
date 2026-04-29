cc000da23576df03e5b2bd2cc2c8410d53975dfb
mpdo1301 <pmd23@uw.edu>
2026-04-29 00:29:27 -0700

    added gps helper

--- App/Inc/task_gps.h
+++ App/Inc/task_gps.h
@@ -1,5 +1,20 @@
-//File: task_gps.h
+// File: task_gps.h
+
+#ifndef TASK_GPS_H
+#define TASK_GPS_H
+
 #include "FreeRTOS.h"
 #include "semphr.h"
+#include <stdint.h>
+#include <stdbool.h>
 
 void taskGPS(void *pvParameters);
+
+void gpsInit(void);
+void sendCommand(const char *cmd);
+
+void startLogging(void);
+void stopLogging(void);
+void dumpLogs(void);
+
+#endif // TASK_GPS_H
